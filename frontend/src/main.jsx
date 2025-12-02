c5fa90cc435bdd9b3dfec749cfa02f481216ebd6
Maxwell. T Ominisan <ominisantimimaxwell@gmail.com>
2025-12-02 12:24:33 -0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // Tailwind CSS import
-import Login from '@/pages/login';
-import Signup from '@/pages/signup';
+import Login from './pages/Login';
+import Signup from './pages/signup';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
